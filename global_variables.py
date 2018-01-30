3f680f451adf28445abb677f2dd0713eb97a1e35
yorik31 <spacesaunier@gmail.com>
2018-01-30 15:16:07 +0100

    Update global variables

--- global_variables.py
+++ global_variables.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
 import os
 
-INFRA_HOME = 'C:\DATA\INFRA'
-INPUT_DATA = 'C:\DATA\INFRA\INPUT'
-CODE = 'C:\Users\saunier\PycharmProjects\LS08_ideas_cyclic_report_processing'
-#CODE_m = '/home/saunier/DEV/LS08_ideas_cyclic_report__processing/CODE/point-m'
-RESULT = '/home/saunier/DEV/LS08_ideas_cyclic_report__processing/RESULT'
-CONFIGURATION = 'C:\DATA\INFRA\CONFIGURATION'
-PROCESSING = 'C:\DATA\INFRA\PROCESSING'
-REFERENCE = 'C:\DATA\INFRA\REFERENCE'
+#WINDOWS CONFIGURATION
+#INFRA_HOME = 'C:\DATA\INFRA'
+#INPUT_DATA = 'C:\DATA\INFRA\INPUT'
+#CODE = 'C:\Users\saunier\PycharmProjects\LS08_ideas_cyclic_report_processing'
+#HTTP_PAGES='/var/www/html/ls08_performance_report'
+#PY_CLASS = 'C:\DEV\LIB\py_class'
+
+#LINUX CONFIGURATION
+INFRA_HOME = '/home/saunier/DEV/LS08_ideas_cyclic_report__processing'
+INPUT_DATA = '/data1/LS8_OLI/'
+CODE = '/home/saunier/DEV/LS08_ideas_cyclic_report__processing/CODE/LS08_ideas_cyclic_report_processing'
+REFERENCE = '/home/saunier/INFRA/REFERENCE'
+HTTP_PAGES='/var/www/html/ls08_performance_report'
+PY_CLASS = '/home/saunier/INFRA/LIB/py_class'
+
+
+CONFIGURATION = os.path.join(INFRA_HOME,'CONFIGURATION')
+PROCESSING = os.path.join(INFRA_HOME,'PROCESSING')
+REFERENCE = os.path.join(INFRA_HOME,'REFERENCE')
 RESULT= os.path.join(INFRA_HOME,'RESULT')
+CODE_m = os.path.join(CODE,'point-m')
+
+#RADIOMETRY
 RADIOMETRIC_STABILITY_RESULTS =  os.path.join(RESULT,'stabilityMonitoring')
 RADIOMETRIC_STABILITY_RHO_RESULTS = os.path.join(RADIOMETRIC_STABILITY_RESULTS,'radio_stability_rho.txt')
 RADIOMETRIC_STABILITY_RAD_RESULTS = os.path.join(RADIOMETRIC_STABILITY_RESULTS,'radio_stability_rad.txt')
-#HTTP_PAGES='/var/www/html/ls08_performance_report'
-PY_CLASS = 'C:\DEV\LIB\py_class'
+#GEOMETRY
 WD_INTERBAND_MS = os.path.join(PROCESSING,'interbandRegistration','wd_ms')
 WD_INTERBAND_PAN = os.path.join(PROCESSING,'interbandRegistration','wd_pan')
 if not os.path.exists(WD_INTERBAND_MS) :
